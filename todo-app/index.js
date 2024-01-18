c9cf63bfd62e2b9dc40f9aad47c49048e886f911
haricharan123461 <151445781+haricharan123461@users.noreply.github.com>
2024-01-18 16:58:47 +0530

    Update index.js

--- todo-app/index.js
+++ todo-app/index.js
@@ -1,6 +1,7 @@
-/* eslint-disable no-undef */
 const app = require("./app");
-const port = process.env.PORT || 3000;
-app.listen(port, () => {
-  console.log(`server listening at port - ${port}`);
+
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT,()=>{
+    console.log('started express server');
 });
